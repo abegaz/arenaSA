4737b342381666be473ba81098eb5878965a6d2e
robccromer <robcaryc@gmail.com>
2017-11-01 21:04:30 -0400

    Added the latest DataBase

--- Work_Space/arenadatabaseSQL11012017.sql
+++ Work_Space/arenadatabaseSQL11012017.sql
@@ -183,16 +183,17 @@ DROP TABLE IF EXISTS `league`;
 CREATE TABLE `league` (
   `LeagueID` int(11) NOT NULL AUTO_INCREMENT,
   `LeagueName` varchar(45) DEFAULT NULL,
+  `leagueDesc` varchar(45) DEFAULT NULL,
   `ExpertRatingFormula_ExpertRatingFormulaID` int(11) NOT NULL,
-  `users_userID` int(10) NOT NULL,
+  `users_userID_LeagueOwner` int(10) NOT NULL,
   `arena_ArenaID` int(11) NOT NULL,
-  PRIMARY KEY (`LeagueID`,`ExpertRatingFormula_ExpertRatingFormulaID`,`users_userID`,`arena_ArenaID`),
+  PRIMARY KEY (`LeagueID`,`ExpertRatingFormula_ExpertRatingFormulaID`,`users_userID_LeagueOwner`,`arena_ArenaID`),
   KEY `fk_League_ExpertRatingFormula1_idx` (`ExpertRatingFormula_ExpertRatingFormulaID`),
   KEY `fk_league_arena1_idx` (`arena_ArenaID`),
-  KEY `fk_league_users1_idx` (`users_userID`),
+  KEY `fk_league_users1_idx` (`users_userID_LeagueOwner`),
   CONSTRAINT `fk_League_ExpertRatingFormula1` FOREIGN KEY (`ExpertRatingFormula_ExpertRatingFormulaID`) REFERENCES `expertratingformula` (`ExpertRatingFormulaID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_league_arena1` FOREIGN KEY (`arena_ArenaID`) REFERENCES `arena` (`ArenaID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_league_users1` FOREIGN KEY (`users_userID`) REFERENCES `users` (`userID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `fk_league_users1` FOREIGN KEY (`users_userID_LeagueOwner`) REFERENCES `users` (`userID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=800004 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -202,7 +203,7 @@ CREATE TABLE `league` (
 
 LOCK TABLES `league` WRITE;
 /*!40000 ALTER TABLE `league` DISABLE KEYS */;
-INSERT INTO `league` VALUES (800001,'Example League Name',990001,900000001,50001),(800002,'Example League Test Name',990001,900000005,50001),(800003,'League Test Name Test1',990001,900000001,50001);
+INSERT INTO `league` VALUES (800001,'Example League Name',NULL,990001,900000001,50001),(800002,'Example League Test Name',NULL,990001,900000005,50001),(800003,'League Test Name Test1',NULL,990001,900000001,50001);
 /*!40000 ALTER TABLE `league` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -217,13 +218,12 @@ CREATE TABLE `leaguemembers` (
   `League_LeagueID` int(11) NOT NULL,
   `MembershipStatusCode_MembershipStatusCodeID` int(11) NOT NULL,
   `users_userID` int(10) NOT NULL,
-  `users_userRoleID` int(11) NOT NULL,
-  PRIMARY KEY (`League_LeagueID`,`MembershipStatusCode_MembershipStatusCodeID`,`users_userID`,`users_userRoleID`),
+  PRIMARY KEY (`League_LeagueID`,`MembershipStatusCode_MembershipStatusCodeID`,`users_userID`),
   KEY `fk_LeagueMembers_MembershipStatusCode1_idx` (`MembershipStatusCode_MembershipStatusCodeID`),
-  KEY `fk_leaguemembers_users1_idx` (`users_userID`,`users_userRoleID`),
+  KEY `fk_leaguemembers_users1_idx` (`users_userID`),
   CONSTRAINT `fk_LeagueMembers_League1` FOREIGN KEY (`League_LeagueID`) REFERENCES `league` (`LeagueID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_LeagueMembers_MembershipStatusCode1` FOREIGN KEY (`MembershipStatusCode_MembershipStatusCodeID`) REFERENCES `membershipstatuscode` (`MembershipStatusCodeID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_leaguemembers_users1` FOREIGN KEY (`users_userID`, `users_userRoleID`) REFERENCES `users` (`userID`, `userRoleID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `fk_leaguemembers_users1` FOREIGN KEY (`users_userID`) REFERENCES `users` (`userID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -250,17 +250,20 @@ CREATE TABLE `match` (
   `MatchStatus_MatchStatusID` int(11) NOT NULL,
   `Game_GameID` int(11) NOT NULL,
   `Tournament_TournamentID` int(11) NOT NULL,
-  `Tournament_Arena_ArenaID` int(11) NOT NULL,
-  `Tournament_League_LeagueID` int(11) NOT NULL,
-  `Tournament_TournamentStyleCode_TournamentStyleCodeID` int(11) NOT NULL,
   `matchDate` datetime DEFAULT NULL,
-  PRIMARY KEY (`MatchID`,`MatchStatus_MatchStatusID`,`Game_GameID`,`Tournament_TournamentID`,`Tournament_Arena_ArenaID`,`Tournament_League_LeagueID`,`Tournament_TournamentStyleCode_TournamentStyleCodeID`),
+  `teams_TeamID2` int(11) NOT NULL,
+  `teams_TeamID1` int(11) NOT NULL,
+  PRIMARY KEY (`MatchID`,`MatchStatus_MatchStatusID`,`Game_GameID`,`Tournament_TournamentID`,`teams_TeamID2`,`teams_TeamID1`),
   UNIQUE KEY `MatchID_UNIQUE` (`MatchID`),
   KEY `fk_Match_MatchStatus1_idx` (`MatchStatus_MatchStatusID`),
-  KEY `fk_Match_Tournament1_idx` (`Tournament_TournamentID`,`Tournament_Arena_ArenaID`,`Tournament_League_LeagueID`,`Tournament_TournamentStyleCode_TournamentStyleCodeID`),
   KEY `fk_Match_Game_idx` (`Game_GameID`),
+  KEY `fk_Match_Tournament1_idx` (`Tournament_TournamentID`),
+  KEY `fk_match_teams1_idx` (`teams_TeamID2`),
+  KEY `fk_match_teams2_idx` (`teams_TeamID1`),
   CONSTRAINT `fk_Match_Game` FOREIGN KEY (`Game_GameID`) REFERENCES `game` (`GameID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Match_MatchStatus1` FOREIGN KEY (`MatchStatus_MatchStatusID`) REFERENCES `matchstatus` (`MatchStatusID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `fk_Match_MatchStatus1` FOREIGN KEY (`MatchStatus_MatchStatusID`) REFERENCES `matchstatus` (`MatchStatusID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_match_teams1` FOREIGN KEY (`teams_TeamID2`) REFERENCES `teams` (`TeamID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_match_teams2` FOREIGN KEY (`teams_TeamID1`) REFERENCES `teams` (`TeamID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -271,72 +274,6 @@ CREATE TABLE `match` (
 LOCK TABLES `match` WRITE;
 /*!40000 ALTER TABLE `match` DISABLE KEYS */;
 /*!40000 ALTER TABLE `match` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `matchparticipants`
---
-
-DROP TABLE IF EXISTS `matchparticipants`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `matchparticipants` (
-  `MatchParticipantsID` int(11) NOT NULL,
-  `Match_MatchID` int(11) NOT NULL,
-  `users_userID` int(10) NOT NULL,
-  `users_userRoleID` int(11) NOT NULL,
-  `match Score` int(11) DEFAULT NULL,
-  PRIMARY KEY (`MatchParticipantsID`,`Match_MatchID`,`users_userID`,`users_userRoleID`),
-  KEY `fk_MatchParticipants_Match1_idx` (`Match_MatchID`),
-  KEY `fk_matchparticipants_users1_idx` (`users_userID`,`users_userRoleID`),
-  CONSTRAINT `fk_MatchParticipants_Match1` FOREIGN KEY (`Match_MatchID`) REFERENCES `match` (`MatchID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_matchparticipants_users1` FOREIGN KEY (`users_userID`, `users_userRoleID`) REFERENCES `users` (`userID`, `userRoleID`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `matchparticipants`
---
-
-LOCK TABLES `matchparticipants` WRITE;
-/*!40000 ALTER TABLE `matchparticipants` DISABLE KEYS */;
-/*!40000 ALTER TABLE `matchparticipants` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `matchscores`
---
-
-DROP TABLE IF EXISTS `matchscores`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `matchscores` (
-  `match_MatchScores_date` datetime NOT NULL,
-  `match_MatchID` int(11) NOT NULL,
-  `game_GameID` int(11) NOT NULL,
-  `User1_userID` int(10) NOT NULL,
-  `User2_userID` int(10) NOT NULL,
-  `User1_Score` varchar(45) DEFAULT NULL,
-  `User2_Score` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`match_MatchScores_date`,`match_MatchID`,`game_GameID`,`User1_userID`,`User2_userID`),
-  KEY `fk_MatchScores_game1_idx` (`game_GameID`),
-  KEY `fk_MatchScores_users1_idx` (`User1_userID`),
-  KEY `fk_MatchScores_users1_idx1` (`User2_userID`),
-  KEY `fk_MatchScores_match1` (`match_MatchID`),
-  CONSTRAINT `fk_MatchScores_game1` FOREIGN KEY (`game_GameID`) REFERENCES `game` (`GameID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_MatchScores_match1` FOREIGN KEY (`match_MatchID`) REFERENCES `match` (`MatchID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_MatchScores_users1` FOREIGN KEY (`User1_userID`) REFERENCES `users` (`userID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_MatchScores_users2` FOREIGN KEY (`User2_userID`) REFERENCES `users` (`userID`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `matchscores`
---
-
-LOCK TABLES `matchscores` WRITE;
-/*!40000 ALTER TABLE `matchscores` DISABLE KEYS */;
-/*!40000 ALTER TABLE `matchscores` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -388,6 +325,101 @@ LOCK TABLES `membershipstatuscode` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `pending`
+--
+
+DROP TABLE IF EXISTS `pending`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pending` (
+  `appID` int(11) NOT NULL AUTO_INCREMENT,
+  `appUserID` int(10) NOT NULL,
+  `appCurrentRole` int(11) NOT NULL,
+  `desired_RoleID` int(11) NOT NULL,
+  PRIMARY KEY (`appID`,`appUserID`,`appCurrentRole`,`desired_RoleID`),
+  KEY `fk_pending_users1_idx` (`appUserID`,`appCurrentRole`),
+  KEY `fk_pending_userroletype1_idx` (`desired_RoleID`),
+  CONSTRAINT `fk_pending_userroletype1` FOREIGN KEY (`desired_RoleID`) REFERENCES `userroletype` (`userRoleTypeID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_pending_users1` FOREIGN KEY (`appUserID`, `appCurrentRole`) REFERENCES `users` (`userID`, `userRoleID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pending`
+--
+
+LOCK TABLES `pending` WRITE;
+/*!40000 ALTER TABLE `pending` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pending` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `scores`
+--
+
+DROP TABLE IF EXISTS `scores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `scores` (
+  `ScoreID` int(11) NOT NULL AUTO_INCREMENT,
+  `match_MatchID` int(11) NOT NULL,
+  `game_GameID` int(11) NOT NULL,
+  `Team1_Score` varchar(45) DEFAULT NULL,
+  `Team2_Score` varchar(45) DEFAULT NULL,
+  `teams_TeamID2` int(11) NOT NULL,
+  `teams_TeamID1` int(11) NOT NULL,
+  PRIMARY KEY (`ScoreID`,`match_MatchID`,`game_GameID`,`teams_TeamID2`,`teams_TeamID1`),
+  UNIQUE KEY `ScoreID_UNIQUE` (`ScoreID`),
+  KEY `fk_MatchScores_game1_idx` (`game_GameID`),
+  KEY `fk_MatchScores_match1` (`match_MatchID`),
+  KEY `fk_scores_teams1_idx` (`teams_TeamID2`),
+  KEY `fk_scores_teams2_idx` (`teams_TeamID1`),
+  CONSTRAINT `fk_MatchScores_game1` FOREIGN KEY (`game_GameID`) REFERENCES `game` (`GameID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_MatchScores_match1` FOREIGN KEY (`match_MatchID`) REFERENCES `match` (`MatchID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_scores_teams1` FOREIGN KEY (`teams_TeamID2`) REFERENCES `teams` (`TeamID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_scores_teams2` FOREIGN KEY (`teams_TeamID1`) REFERENCES `teams` (`TeamID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `scores`
+--
+
+LOCK TABLES `scores` WRITE;
+/*!40000 ALTER TABLE `scores` DISABLE KEYS */;
+/*!40000 ALTER TABLE `scores` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `teams`
+--
+
+DROP TABLE IF EXISTS `teams`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `teams` (
+  `TeamID` int(11) NOT NULL,
+  `TeamName` varchar(45) DEFAULT NULL,
+  `users_userID` int(10) NOT NULL,
+  `league_LeagueID` int(11) NOT NULL,
+  PRIMARY KEY (`TeamID`,`users_userID`,`league_LeagueID`),
+  KEY `fk_teams_users1_idx` (`users_userID`),
+  KEY `fk_teams_league1_idx` (`league_LeagueID`),
+  CONSTRAINT `fk_teams_league1` FOREIGN KEY (`league_LeagueID`) REFERENCES `league` (`LeagueID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_teams_users1` FOREIGN KEY (`users_userID`) REFERENCES `users` (`userID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `teams`
+--
+
+LOCK TABLES `teams` WRITE;
+/*!40000 ALTER TABLE `teams` DISABLE KEYS */;
+/*!40000 ALTER TABLE `teams` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `tournament`
 --
 
@@ -395,22 +427,19 @@ DROP TABLE IF EXISTS `tournament`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tournament` (
-  `TournamentID` int(11) NOT NULL,
+  `TournamentID` int(11) NOT NULL AUTO_INCREMENT,
   `TournamentName` varchar(45) DEFAULT NULL,
   `TournamentDescription` varchar(45) DEFAULT NULL,
-  `Arena_ArenaID` int(11) NOT NULL,
   `League_LeagueID` int(11) NOT NULL,
   `TournamentStyleCode_TournamentStyleCodeID` int(11) NOT NULL,
-  `tournamentDate` datetime DEFAULT NULL,
-  PRIMARY KEY (`TournamentID`,`Arena_ArenaID`,`League_LeagueID`,`TournamentStyleCode_TournamentStyleCodeID`),
+  `tournamentDate` date DEFAULT NULL,
+  PRIMARY KEY (`TournamentID`,`League_LeagueID`,`TournamentStyleCode_TournamentStyleCodeID`),
   UNIQUE KEY `TournamentID_UNIQUE` (`TournamentID`),
   KEY `fk_Tournament_League1_idx` (`League_LeagueID`),
   KEY `fk_Tournament_TournamentStyleCode1_idx` (`TournamentStyleCode_TournamentStyleCodeID`),
-  KEY `fk_tournament_arena1_idx` (`Arena_ArenaID`),
   CONSTRAINT `fk_Tournament_League1` FOREIGN KEY (`League_LeagueID`) REFERENCES `league` (`LeagueID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Tournament_TournamentStyleCode1` FOREIGN KEY (`TournamentStyleCode_TournamentStyleCodeID`) REFERENCES `tournamentstylecode` (`TournamentStyleCodeID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_tournament_arena1` FOREIGN KEY (`arena_ArenaID`) REFERENCES `arena` (`ArenaID`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  CONSTRAINT `fk_Tournament_TournamentStyleCode1` FOREIGN KEY (`TournamentStyleCode_TournamentStyleCodeID`) REFERENCES `tournamentstylecode` (`TournamentStyleCodeID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=1000001 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -419,6 +448,7 @@ CREATE TABLE `tournament` (
 
 LOCK TABLES `tournament` WRITE;
 /*!40000 ALTER TABLE `tournament` DISABLE KEYS */;
+INSERT INTO `tournament` VALUES (1000000,'Test Tournament','Test Tournament Description',800001,40001,'2000-01-01');
 /*!40000 ALTER TABLE `tournament` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -443,6 +473,7 @@ CREATE TABLE `tournamentstylecode` (
 
 LOCK TABLES `tournamentstylecode` WRITE;
 /*!40000 ALTER TABLE `tournamentstylecode` DISABLE KEYS */;
+INSERT INTO `tournamentstylecode` VALUES (40001,'Single Elimination','Single Elimination');
 /*!40000 ALTER TABLE `tournamentstylecode` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -487,11 +518,10 @@ CREATE TABLE `users` (
   PRIMARY KEY (`userID`,`userRoleID`),
   UNIQUE KEY `userID_UNIQUE` (`userID`),
   UNIQUE KEY `userName_UNIQUE` (`userName`),
-  UNIQUE KEY `userPassword_UNIQUE` (`userPassword`),
   KEY `RoleTypeID_idx` (`userRoleID`),
   KEY `fk_users_userroletype1_idx` (`userRoleID`),
   CONSTRAINT `fk_users_userroletype1` FOREIGN KEY (`userRoleID`) REFERENCES `userroletype` (`userRoleTypeID`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=900000011 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=900000018 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -500,7 +530,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (900000000,'Admin','Password',0),(900000001,'LeagueOwner','LeagueOwnerPassword ',1),(900000002,'Advertiser','AvertiserPassword',2),(900000003,'Player','PlayerPassword',3),(900000004,'Rob','rob',0),(900000005,'James','james',1),(900000006,'Franky','frank',3),(900000007,'bren','bren',4),(900000008,'Mike','mikey',2),(900000009,'phil','phily',4),(900000010,'phily','phil',4);
+INSERT INTO `users` VALUES (900000000,'Admin','Password',0),(900000001,'LeagueOwner','LeagueOwnerPassword ',1),(900000002,'Advertiser','AvertiserPassword',2),(900000003,'Player','PlayerPassword',3),(900000004,'Rob','rob',0),(900000005,'James','james',1),(900000006,'Franky','frank',3),(900000007,'bren','bren',4),(900000008,'Mike','mikey',2),(900000009,'phil','phily',4),(900000010,'phily','phil',4),(900000012,'sam','sammy',4),(900000013,'roboo','robby',1),(900000014,'philyd','philyd',3),(900000016,'tay','tay',2),(900000017,'bob','bob',0);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -513,4 +543,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-10-28 20:25:16
+-- Dump completed on 2017-11-01 21:03:05
