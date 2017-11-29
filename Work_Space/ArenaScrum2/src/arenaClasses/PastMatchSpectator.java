3bbab0376a328cdad93cc3d4ac818e85ba356baf
MCBell <MCBell5177@ung.edu>
2017-11-28 20:58:49 -0500

    fixing query in Past Matches

--- Work_Space/ArenaScrum2/src/arenaClasses/PastMatchSpectator.java
+++ Work_Space/ArenaScrum2/src/arenaClasses/PastMatchSpectator.java
@@ -88,7 +88,7 @@ public class PastMatchSpectator extends Main{
     	data = FXCollections.observableArrayList();
     	data.removeAll(data);
     	try {
-    		ResultSet rs = myConnection.createStatement().executeQuery("Select M.MatchID, G.GameName, L.LeagueName, T.TournamentName, M.teams_TeamID1, M.teams_TeamID2, S.Team1_Score, S.Team2_Score FROM league AS L, tournament AS T, scores AS S, teams AS A, teams AS B, matches AS M, game AS G WHERE M.Game_GameID=G.GameID and S.match_MatchID = M.MatchID and L.LeagueID=T.League_LeagueID and T.TournamentID=M.Tournament_TournamentID and A.TeamID=M.teams_TeamID1 and B.TeamID=M.teams_TeamID2 and M.MatchStatus_MatchStatusID=2 and ScoreStatusTable_idScoreStatusTable<>1 and ScoreStatusTable_idScoreStatusTable<>3 and ScoreStatusTable_idScoreStatusTable<>4");
+    		ResultSet rs = myConnection.createStatement().executeQuery("Select M.MatchID, G.GameName, L.LeagueName, T.TournamentName, M.teams_TeamID1, M.teams_TeamID2, S.Team1_Score, S.Team2_Score FROM league AS L, tournament AS T, scores AS S, teams AS A, teams AS B, matches AS M, game AS G WHERE M.Game_GameID=G.GameID and S.match_MatchID = M.MatchID and L.LeagueID=T.League_LeagueID and T.TournamentID=M.Tournament_TournamentID and A.TeamID=M.teams_TeamID1 and B.TeamID=M.teams_TeamID2 and M.MatchStatus_MatchStatusID=2 and S.ScoreStatusTable_idScoreStatusTable<>1 and S.ScoreStatusTable_idScoreStatusTable<>3 and S.ScoreStatusTable_idScoreStatusTable<>5 and S.ScoreID = (SELECT Max(S1.ScoreID) FROM scores S1 WHERE S1.match_MatchID=S.match_MatchID)");
     		while (rs.next()){
     			String teamname1 = getTeamNameWithTeamID(rs.getString("teams_TeamID1"));
     			String teamname2 = getTeamNameWithTeamID(rs.getString("teams_TeamID2"));
@@ -122,7 +122,7 @@ public class PastMatchSpectator extends Main{
     	data.removeAll(data);
     	
     	try {
-    		ResultSet rs = myConnection.createStatement().executeQuery("Select M.MatchID, G.GameName, L.LeagueName, T.TournamentName, M.teams_TeamID1, M.teams_TeamID2, S.Team1_Score, S.Team2_Score FROM league AS L, tournament AS T, scores AS S, teams AS A, teams AS B, matches AS M, game AS G WHERE M.Game_GameID=G.GameID and S.match_MatchID = M.MatchID and L.LeagueID=T.League_LeagueID and T.TournamentID=M.Tournament_TournamentID and A.TeamID=M.teams_TeamID1 and B.TeamID=M.teams_TeamID2 and M.MatchStatus_MatchStatusID=2 and ScoreStatusTable_idScoreStatusTable<>1 and ScoreStatusTable_idScoreStatusTable<>3 and ScoreStatusTable_idScoreStatusTable<>4");
+    		ResultSet rs = myConnection.createStatement().executeQuery("Select M.MatchID, G.GameName, L.LeagueName, T.TournamentName, M.teams_TeamID1, M.teams_TeamID2, S.Team1_Score, S.Team2_Score FROM league AS L, tournament AS T, scores AS S, teams AS A, teams AS B, matches AS M, game AS G WHERE M.Game_GameID=G.GameID and S.match_MatchID = M.MatchID and L.LeagueID=T.League_LeagueID and T.TournamentID=M.Tournament_TournamentID and A.TeamID=M.teams_TeamID1 and B.TeamID=M.teams_TeamID2 and M.MatchStatus_MatchStatusID=2 and S.ScoreStatusTable_idScoreStatusTable<>1 and S.ScoreStatusTable_idScoreStatusTable<>3 and S.ScoreStatusTable_idScoreStatusTable<>5 and S.ScoreID = (SELECT Max(S1.ScoreID) FROM scores S1 WHERE S1.match_MatchID=S.match_MatchID)");
     		while (rs.next()){
         		String teamname1 = getTeamNameWithTeamID(rs.getString("teams_TeamID1"));
     			String teamname2 = getTeamNameWithTeamID(rs.getString("teams_TeamID2"));
